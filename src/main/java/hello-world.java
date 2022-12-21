a6a8a33c34c6e37fd7d086a17a14e6b602f109d6
KamalaSelvaraj <121024604+KamalaSelvaraj@users.noreply.github.com>
2022-12-21 18:14:48 +0530

    Update hello-world.java

--- src/main/java/hello-world.java
+++ src/main/java/hello-world.java
@@ -1,7 +1,5 @@
-public class Helloworld{
-	public static void main(string[]args)
-	{
-		system.out.println("hello World");
-	}
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
-
